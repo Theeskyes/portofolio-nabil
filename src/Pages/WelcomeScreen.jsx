bd548de85091c304a40db862b9e8ec7ca1d713e4
Theeskyes <galaksiputra2009@gmail.com>
2026-05-28 20:31:28 +0700

    update 9

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -16,7 +16,7 @@ const TypewriterEffect = ({ text }) => {
       } else {
         clearInterval(timer);
       }
-    }, 260);
+    }, 260);  
     
     return () => clearInterval(timer);
   }, [text]);
@@ -152,7 +152,7 @@ const WelcomeScreen = ({ onLoadingComplete }) => {
                 data-aos-delay="1200"
               >
                 <a
-                  href="https://ekizr.com"
+                  href="portofolio-kenzie3.vercel.app"
                   className="inline-flex items-center gap-2 px-4 py-2 sm:px-6 sm:py-3 rounded-full relative group hover:scale-105 transition-transform duration-300"
                   target="_blank"
                   rel="noopener noreferrer"
