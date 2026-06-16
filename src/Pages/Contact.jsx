5deaac3f7a5821f5a8510bff3e3d5a3c57e13e85
Theeskyes <galaksiputra2009@gmail.com>
2026-06-16 22:17:13 +0700

    revisi sama aja

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -107,7 +107,7 @@ const ContactPage = () => {
   };
 
   return (
-    <div className="px-[5%] sm:px-[5%] lg:px-[10%] " >
+    <div className="px-[5%] sm:px-[5%] lg:px-[5%]">
       <div className="text-center lg:mt-[5%] mt-10 mb-2 sm:px-0 px-[5%]">
         <h2
           data-aos="fade-down"
